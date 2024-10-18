841db18d993e8b4fa928c643ff93bf9c23f36e74
zhilanazmi <zhilannazmi@gmail.com>
2024-10-18 23:46:15 +0700

    feat: add card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,96 @@
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 22,
+              ),
+            _greetings(),
+            const SizedBox(
+              height: 17,
+              ),
+            _card()
+        ],),),
+    );
+  }
+
+  AspectRatio _card() {
+    return AspectRatio(
+            aspectRatio: 336/184,
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                color: const Color(0xFF818AF9)
+                ),
+                child: 
+                Stack(
+                  children: [Image.asset('assets/images/bgcard.png', 
+                  height: double.maxFinite,
+                  width: double.maxFinite,
+                  fit: BoxFit.cover,
+                  )]
+                ),
+            ),
+          );
+  }
+
+  Padding _greetings() {
+    return Padding(
+            padding: const  EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row( 
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text
+                ('Hello, Human!', 
+                  style: GoogleFonts.manrope(
+                    fontSize: 24, 
+                    fontWeight:  FontWeight.w800,
+                    color: const Color(0xFF3F3E3F),
+                  ) 
+                ),
+
+                Stack(
+                  children: [
+                    IconButton(
+                    onPressed: () {}, 
+                    icon: const Icon(
+                      FeatherIcons.shoppingBag, 
+                      color: Color(0xFF818AF9 ),
+                    )),
+                  Positioned(
+                    right: 8,
+                    top: 8,
+                    child: Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFEF6497), 
+                        borderRadius: BorderRadius.circular(15/2)
+                        ),
+                      child: Center(child: Text('2', style: GoogleFonts.mPlus1p(
+                        color: Colors.white, 
+                        fontSize: 10, 
+                          fontWeight: FontWeight.w800),
+                      )),
+                    ),
+                  )
+                  ])
+            ],
+            ),
+          );
   }
 }
